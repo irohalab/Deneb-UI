107ba46b67aa53411295b42ee42cb3221971e67f
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2023-01-20 07:10:35 -0800

    Add global listener for dark-theme.service.ts two sync between multiple instance of DarkThemeService

--- projects/irohalab/deneb-ui/src/dark-theme.service.ts
+++ projects/irohalab/deneb-ui/src/dark-theme.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 export const DARK_THEME = 'dark_theme';
@@ -6,9 +6,11 @@ export const LIGHT_THEME = 'light_theme';
 
 const THEME_FOR_DENEB = 'theme_for_deneb';
 export type ThemeName = 'dark_theme' | 'light_theme';
+const EVENT_NAME = 'DenebThemeChange';
 
 @Injectable()
-export class DarkThemeService {
+export class DarkThemeService implements OnDestroy {
+    private _globalListenerInstance: () => void;
     private _themeChangeSubject = new BehaviorSubject<string>(null);
 
     public get themeChange(): Observable<string> {
@@ -16,18 +18,35 @@ export class DarkThemeService {
     }
 
     constructor() {
-        const theme = window.localStorage.getItem(THEME_FOR_DENEB);
-        if (theme === DARK_THEME || theme === LIGHT_THEME) {
-            this._themeChangeSubject.next(theme);
-        }
+        this.checkTheme();
+        this.initGlobalListener();
     }
 
     public changeTheme(theme: ThemeName): void {
         window.localStorage.setItem(THEME_FOR_DENEB, theme);
         this._themeChangeSubject.next(theme);
+        const event = new CustomEvent(EVENT_NAME);
+        window.dispatchEvent(event);
     }
 
     public getCurrentTheme(): ThemeName {
         return this._themeChangeSubject.value as ThemeName;
+    }
+
+    public ngOnDestroy(): void {
+        window.removeEventListener(EVENT_NAME, this._globalListenerInstance);
+    }
+
+    private initGlobalListener(): void {
+        this._globalListenerInstance = () => {
+            this.checkTheme();
+        };
+        window.addEventListener(EVENT_NAME, this._globalListenerInstance);
+    }
+    private checkTheme(): void {
+        const theme = window.localStorage.getItem(THEME_FOR_DENEB);
+        if (theme === DARK_THEME || theme === LIGHT_THEME) {
+            this._themeChangeSubject.next(theme);
+        }
     }
 }
