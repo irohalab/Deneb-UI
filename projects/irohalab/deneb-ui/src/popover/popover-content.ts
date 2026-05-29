e90c03a89e3629ab503b77c9390c0f27a9af4de2
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2026-05-29 17:52:14 +0100

    fix migration issue.

--- projects/irohalab/deneb-ui/src/popover/popover-content.ts
+++ projects/irohalab/deneb-ui/src/popover/popover-content.ts
@@ -1,14 +1,14 @@
 /**
  * All popover content component should extend this class
  */
-import { afterRender, Directive } from '@angular/core';
+import { afterEveryRender, Directive } from '@angular/core';
 import { UIPopoverRef } from './popover-ref';
 
 @Directive()
 export abstract class UIPopoverContent{
 
     protected constructor(protected popoverRef: UIPopoverRef<UIPopoverContent>) {
-        afterRender(() => {
+        afterEveryRender(() => {
             this.popoverRef.updatePosition();
         });
     }
